9e4bbc5714683ce07f3addb1d586510950ce5acb
Oscar L. Rodriguez <olrodriguez01@gmail.com>
2024-12-20 16:55:22 -0500

    Update run.sh

--- results/2024-12-10_run_avocado/run.sh
+++ results/2024-12-10_run_avocado/run.sh
@@ -6,15 +6,16 @@ scratch=/sc/arion/scratch/arayan01/projects/r35_2025/results/2024-12-10_run_avoc
 data_dir="../../data/2024-12-04_encode_data"
 
 
-function transform_bigwig_files {
-  mkdir -p ${scratch}/transform_bigwig_files
+function transform_to_wig {
+  mkdir -p ${scratch}/transform_to_wig
   cat ../../data/2024-12-04_encode_data/url_links.txt | while read sample assay link
   do
       # get file name from $link
       fn=`cat $link | rev | cut -f1 -d/ | rev`
-      python bigwig_conversion.py \
+      bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
+          "python big_to_wig.py \
           ../../data/2024-12-04_encode_data/${sample}/${assay}/${fn}
-          ${scratch}/transform_bigwig_files/${sample}_${assay}.txt.gz
+          ${scratch}/transform_to_wig/${sample}_${assay}.wig"
 }
 
 function running_avocado {
@@ -22,4 +23,4 @@ function running_avocado {
 
 }
 
-transform_bigwig_files
+transform_to_wig
