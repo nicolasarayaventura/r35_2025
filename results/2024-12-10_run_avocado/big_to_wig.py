c241b61e6a7ff8dcfa0b629029709d9bc56df491
Nicolas Araya Ventura <arayan01@li04e01.chimera.hpc.mssm.edu>
2024-12-20 16:02:48 -0500

    edit

--- results/2024-12-10_run_avocado/big_to_wig.py
+++ results/2024-12-10_run_avocado/big_to_wig.py
@@ -1,20 +1,15 @@
+import os
 import pyBigWig
-import os 
 
-# Define directories
-input_dir = "/sc/arion/work/arayan01/project/r35_2025/data/2024-12-04_encode_data"
-output_dir = "/sc/arion/scratch/arayan01/projects/r35_2025/data/2024-12-04_encode_data/bigwig_towig"
+work_data = "/sc/arion/work/arayan01/project/r35_2025/data/2024-12-04_encode_data"
+scratch = "/sc/arion/scratch/arayan01/project/r35_2025/data/2024-12-04_encode_data/bigwig_towig"
 
-def bigwig_to_wig(bw_file, scratch):
-    # Open the BigWig file
+def bigwig_to_wig(bw_file, output_file, chrom="chr14"):
     bw = pyBigWig.open(bw_file)
-    
-    with open(scratch, 'w') as wig:
+    with open(output_file, 'w') as wig:
         # Write the header for the Wig file
         wig.write("track type=wiggle_0\n")
-        
-        # Iterate over chromosomes
-        for chrom in bw.chroms():
+        if chrom in bw.chroms():
             start = 0
             end = bw.chroms()[chrom]
             values = bw.values(chrom, start, end)
@@ -23,12 +18,13 @@ def bigwig_to_wig(bw_file, scratch):
             for i, value in enumerate(values):
                 if value != float('nan'):  # Skip NaN values
                     wig.write(f"{chrom}\t{i}\t{i+1}\t{value}\n")
+        else:
+            print(f"Chromosome {chrom} not found in the BigWig file.")
     
-    # Close the BigWig file
     bw.close()
 
 # Iterate over all BigWig files in the directory
-for root, dirs, files in os.walk(input_dir):
+for root, dirs, files in os.walk(work_data):
     for file in files:
         if file.endswith(".bigWig"):
             # Full path to the BigWig file
@@ -37,7 +33,7 @@ for root, dirs, files in os.walk(input_dir):
             # Create the output file name by replacing .bigWig with .wig
             output_file = os.path.join(scratch, f"{os.path.basename(bw_file).replace('.bigWig', '.wig')}")
             
-            # Convert the BigWig file to a Wig file
-            bigwig_to_wig(bw_file, output_file)
+            # Convert the BigWig file to a Wig file for chrom "chr14"
+            bigwig_to_wig(bw_file, output_file, chrom="chr14")
             
             print(f"Converted {bw_file} to {output_file}")
