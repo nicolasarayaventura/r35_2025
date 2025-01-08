699551889c4f5ed9cdd3e157bf036473611adf03
Nicolas Araya Ventura <150105875+nicolasarayaventura@users.noreply.github.com>
2025-01-08 17:01:23 -0500

    p values only in scratch

--- results/2024-12-10_run_avocado/big_to_wig.py
+++ results/2024-12-10_run_avocado/big_to_wig.py
@@ -2,16 +2,9 @@ import os
 import sys
 import pyBigWig
 import numpy as np
-
+#averaged values displayed only
 input_bigwig = sys.argv[1]
 output_wig = sys.argv[2]
-
-# Define the paths for the input directory and the output directory
-#work_data = "/sc/arion/work/arayan01/project/r35_2025/data/2024-12-04_encode_data"
-#scratch = "/sc/arion/scratch/arayan01/project/r35_2025/data/2024-12-04_encode_data/bigwig_towig"
-
-# Ensure the scratch directory exists
-#os.makedirs(scratch, exist_ok=True)
 
 def bigwig_to_wig(bw_file, output_file, chrom="chr14", window_size=25):
     # Open the BigWig file
@@ -36,9 +29,8 @@ def bigwig_to_wig(bw_file, output_file, chrom="chr14", window_size=25):
             ]
             
             # Write the averaged values to the Wig file
-            for i, avg_value in enumerate(averaged_values):
-                wig.write(f"{chrom}\t{i*window_size}\t{i*window_size + window_size}\t{avg_value}\n")
-                
+            for avg_value in averaged_values:
+                 wig.write(f"{avg_value}\n")    
         else:
             print(f"Chromosome {chrom} not found in the BigWig file.")
     
@@ -47,26 +39,5 @@ def bigwig_to_wig(bw_file, output_file, chrom="chr14", window_size=25):
 
 bigwig_to_wig(input_bigwig,output_wig)
 
-'''
-# Iterate over all BigWig files in the directory and subdirectories
-for root, dirs, files in os.walk(work_data):
-    for file in files:
-        if file.endswith(".bigWig"):
-            # Full path to the BigWig file
-            bw_file = os.path.join(root, file)
-            
-            # Replicate the directory structure in scratch
-            relative_path = os.path.relpath(root, work_data)  # Get relative path of the current folder
-            output_dir = os.path.join(scratch, relative_path)  # Create corresponding output directory in scratch
-            os.makedirs(output_dir, exist_ok=True)
-            
-            # Create the output file name by replacing .bigWig with _averaged.wig
-            output_file = os.path.join(output_dir, f"{os.path.basename(bw_file).replace('.bigWig', '_averaged.wig')}")
-            
-            # Convert the BigWig file to a Wig file for chrom "chr14" and compute averages
-            bigwig_to_wig(bw_file, output_file, chrom="chr14")
-            
-            print(f"Converted {bw_file} to {output_file}")
-'''
 
 
