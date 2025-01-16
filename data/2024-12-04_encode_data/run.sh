b22f3719421e6dbb98a720dac8289cb3ec2b41d8
Nicolas Araya Ventura <arayan01@li04e04.chimera.hpc.mssm.edu>
2025-01-16 15:03:45 -0500

    update downloads for manual attempt

--- data/2024-12-04_encode_data/run.sh
+++ data/2024-12-04_encode_data/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x -e
+
 cat url_links.txt | while read sample assay url
 do
 	mkdir -p "${sample}/${assay}"
 	wget -P "${sample}/${assay}" "${url}"
 done < url_links.txt
 
-cat avocado_model.txt | while read model url
-do
-	mkdir -p "${model}"
-	wget -P "${model}" "${url}"
+cat avocado_model.txt | while read model h5_url json_url; do
+    mkdir -p "${model}"
+    wget -P "${model}" "${h5_url}"
+    wget -P "${model}" "${json_url}"
 done < avocado_model.txt
-
