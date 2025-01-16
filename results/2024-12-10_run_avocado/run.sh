049b45068d11e84d5af5c137896cc5ff15ba28b5
Nicolas Araya Ventura <arayan01@li04e01.chimera.hpc.mssm.edu>
2025-01-16 14:09:00 -0500

    update figuring out how to run on single file

--- results/2024-12-10_run_avocado/run.sh
+++ results/2024-12-10_run_avocado/run.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 set -e -x
 
-
-scratch=/sc/arion/scratch/arayan01/projects/r35_2025/results/2024-12-10_run_avocoda
+scratch="/sc/arion/scratch/arayan01/projects/r35_2025/results/2024-12-10_run_avocoda"
 data_dir="../../data/2024-12-04_encode_data"
 conda_env="avocado"
 
+# Transform .big files to .wig files
 function transform_to_wig {
   mkdir -p ${scratch}/transform_to_wig
-  cat ../../data/2024-12-04_encode_data/url_links.txt | while read sample assay link
-  do
-      # get file name from $link
-      fn=`cat $link | rev | cut -f1 -d/ | rev`
-      bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
-          "python big_to_wig.py \
-          ../../data/2024-12-04_encode_data/${sample}/${assay}/${fn}
-          ${scratch}/transform_to_wig/${sample}_${assay}.wig"
-}
-
-function running_avocado {
-  source activate $conda_env
-  python avodo_analysis.py
-  mkdir -p ${scratch}/avocado_analysis
-  do
+  while read sample assay link; do
+    # Get file name from $link
+    fn=$(basename "$link")
     bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
-        "python avocado_analysis.py \
-        ../../data/2024-12-04_encode_data/${sample}/${assay}/${fn} \
-        ${scratch}/avocado_analysis/${sample}_${assay}.wig"
-
+      "python big_to_wig.py \
+      ${data_dir}/${sample}/${assay}/${fn} \
+      ${scratch}/transform_to_wig/4${sample}_${assay}.wig"
+  done < ${data_dir}/url_links.txt
+}
+  #test singular file first!
+function test {
+  mkdir -p ${scratch}/test_avo
+     bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
+      "python single_avo.py \
+          ${scratch}/transform_to_wig/4${sample}_${assay}.wig \
+          ${scratch}/test_avo/4${sample}_${assay}_test.wig"
+      done
+}
+function running_avocado {
+  mkdir -p ${scratch}/avocado_analysis
+     bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
+      "python avocado_analysis.py \
+          ${scratch}/transform_to_wig/4${sample}_${assay}.wig \
+          ${scratch}/avocado_analysis/4${sample}_${assay}.wig"
+      done
 }
 
-transform_to_wig
+test
+#transform_to_wig
+#running_avocado
+
