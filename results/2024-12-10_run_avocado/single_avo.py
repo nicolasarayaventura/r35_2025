b695f3aa1295c6e3b82a2637864c26b9dd7f17ef
Nicolas Araya Ventura <arayan01@li04e04.chimera.hpc.mssm.edu>
2025-01-17 13:40:37 -0500

    fixed issues but now my wig file doesnt contain enough data?

--- results/2024-12-10_run_avocado/single_avo.py
+++ results/2024-12-10_run_avocado/single_avo.py
@@ -1,26 +1,44 @@
-import os
-import glob
-import subprocess
-import numpy
-from tensorflow import keras
+import numpy as np
 from avocado import Avocado
 
-wig_files = sys.argv[1]
-avocado_analysis = sys.argv[2]
+# Function to read WIG file
+def read_wig_file(filename):
+    positions = []
+    values = []
+    with open(filename, 'r') as f:
+        for line in f:
+            if line.startswith('track') or line.startswith('browser'):
+                continue
+            parts = line.strip().split()
+            if len(parts) == 2:
+                positions.append(parts[0])
+                values.append(parts[1])
+    return positions, values
 
-model = Avocado.load("avocado-chr14")
-data = {}
-
-assays = ["H3K36me3", "H3K27me3"]
-
+# Set paths
 scratch = "/sc/arion/scratch/arayan01/projects/r35_2025/results/2024-12-10_run_avocoda"
 sample_name = "GM06690"
+assays = ["H3K36me3", "H3K27me3"]
+model_path = "/sc/arion/work/arayan01/project/r35_2025/data/2024-12-04_encode_data/avocado-chr14/avocado-chr14"
 
+# Initialize data dictionary
+data = {}
+
+# Load data for each assay
 for assay in assays:
-    filename = "%s/transform_to_wig/%s_%s" % (scratch, sample_name, assay)  # Use straight quotes here
-    data[(sample_name, assay)] = numpy.load(filename)['arr_0']
+    wig_filename = "%s/transform_to_wig/%s_%s.wig" % (scratch, sample_name, assay)  # .wig files instead of .npy
+    positions, values = read_wig_file(wig_filename)  # Read the .wig file
+    data[(sample_name, assay)] = (positions, values)  # Store positions and values
 
+# Load the Avocado model
+model = Avocado.load(model_path)
+
+# Train the model on the loaded data
 model.fit_celltypes(data, n_epochs=5)
 
-track = model.predict(sample_name, "H3K4me3")  # Use straight quotes here as well
+# Make a prediction for the H3K4me3 assay
+track = model.predict(sample_name, "H3K4me3")
+
+# Print the prediction results
+print(f"Prediction for {sample_name} H3K4me3: {track}")
 
