615a698010e0f7001503df3743ca39d0d2502190
Nicolas Araya Ventura <arayan01@li04e01.chimera.hpc.mssm.edu>
2025-01-21 17:03:53 -0500

    edit

--- results/2024-12-10_run_avocado/2run.sh
+++ results/2024-12-10_run_avocado/2run.sh
@@ -11,10 +11,10 @@ function transform_to_wig {
   while read sample assay link; do
     # Get file name from $link
     fn=$(basename "$link")
-	bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=5000]" -o job.txt \
-  		"python 2big_to_wig.py \
-  		${data_dir}/${sample}/${assay}/${fn} \
-  		${scratch}/transform_to_wig/4${sample}_${assay}.wig"
+    bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=5000]" -o job.txt \
+      "python 2big_to_wig.py \
+      ${data_dir}/${sample}/${assay}/${fn} \
+      ${scratch}/transform_to_wig/4${sample}_${assay}.wig"
   done < ${data_dir}/url_links.txt
 }
 
@@ -25,7 +25,7 @@ function test {
   # Set the specific file for testing
   sample="GM06690"
   assay="H3K27me3"
-  input_file="${scratch}/transform_to_wig/4${sample}_${assay}.wig"
+  input_file="${scratch}/transform_to_wig/${sample}_${assay}.wig"
   output_file="${scratch}/test_avo/4${sample}_${assay}_test.wig"
 
   bsub -P acc_oscarlr -q gpu -n 1 -W 24:00 -o job.txt \
@@ -35,15 +35,26 @@ function test {
 }
 
 # Running avocado analysis
-function running_avocado {
-  mkdir -p ${scratch}/avocado_analysis
-  bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -o job.txt \
-    "python avocado_analysis.py \
-        ${scratch}/transform_to_wig/4${sample}_${assay}.wig \
-        ${scratch}/avocado_analysis/4${sample}_${assay}.wig"
+function run_avo {
+  # Define the samples array
+  samples=("GM06690_H3K4me3" "GM23248_H3K36me3")
+
+  rm -rf ${scratch}/avo_analysis
+  mkdir -p ${scratch}/avo_analysis
+
+  for sample in "${samples[@]}"; do
+    assay=$(echo $sample | awk -F'_' '{print $2}')
+    input_wig="${scratch}/transform_to_wig/${sample}_${assay}.wig"
+    output_predictions="${scratch}/test_avo/4${sample}_${assay}_test.wig"
+
+    mkdir -p "${scratch}/test_avo"
+
+	bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=6000]" -o "${scratch}/avo_analysis/job_${sample}.txt" \
+ 	 "python run_avo.py ${input_wig} ${output_predictions}"
+
+  done
 }
 
 #transform_to_wig
-test
-# running_avocado
-
+#test
+run_avo
