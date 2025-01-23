4601c93feb18dafffa5980f40a50b2bd3fb8fc6f
Nicolas Araya Ventura <arayan01@li04e03.chimera.hpc.mssm.edu>
2025-01-23 16:11:14 -0500

    added extra functions for plots

--- results/2024-12-10_run_avocado/2run.sh
+++ results/2024-12-10_run_avocado/2run.sh
@@ -28,7 +28,7 @@ function test {
   input_file="${scratch}/transform_to_wig/${sample}_${assay}.wig"
   output_file="${scratch}/test_avo/4${sample}_${assay}_test.wig"
 
-  bsub -P acc_oscarlr -q gpu -n 1 -W 24:00 -o job.txt \
+  bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=8000]" -o job.txt \
     "python single_avo.py \
         ${input_file} \
         ${output_file}"
@@ -45,16 +45,63 @@ function run_avo {
   for sample in "${samples[@]}"; do
     assay=$(echo $sample | awk -F'_' '{print $2}')
     input_wig="${scratch}/transform_to_wig/${sample}_${assay}.wig"
-    output_predictions="${scratch}/test_avo/4${sample}_${assay}_test.wig"
+    output_predictions="${scratch}/test_avo/4${sample}_test.txt"
 
     mkdir -p "${scratch}/test_avo"
 
-	bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=6000]" -o "${scratch}/avo_analysis/job_${sample}.txt" \
+	bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=6000]" -o "${scratch}/avo_analysis/job_${sample}_run_avo.txt" \
  	 "python run_avo.py ${input_wig} ${output_predictions}"
 
   done
 }
+function test_plot {
+  mkdir -p ${scratch}/test_avo
+  plot_dir="${scratch}/plots"
+  mkdir -p ${plot_dir}
+
+  # Set the specific file for testing
+  sample="GM06690"
+  assay="H3K27me3"
+  input_file="${scratch}/transform_to_wig/4${sample}_${assay}.wig"
+  output_file="${scratch}/test_avo/4${sample}_${assay}_test.wig"
+
+  bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=6000]" -o job_plots.txt \
+    "python single_plots_avo.py \
+        ${input_file} \
+        ${output_file} \
+        --plot-dir ${plot_dir}"
+}
+function test_multi_plot {
+  plot_dir="${scratch}/plots"
+  mkdir -p ${plot_dir}
+
+  # List of sample names and assays
+  samples=("GM06690" "GM23248")
+  assays=("H3K4me3" "H3K36me3" "H3K27me3")
+
+  # Loop over each sample and assay to process
+  for sample_name in "${samples[@]}"; do
+    for assay in "${assays[@]}"; do
+      # Set input and output file paths
+      input_file="${scratch}/transform_to_wig/${sample_name}_${assay}.wig"
+      output_file="${scratch}/test_avo/${sample_name}_${assay}_test.wig"
+      output_plot_file="${plot_dir}/${sample_name}_${assay}_imputation.png"
+
+      # Submit the job using bsub
+      bsub -P acc_oscarlr -q premium -n 1 -W 24:00 -R "rusage[mem=6000]" -o job_${sample_name}_${assay}_multiplot.txt \
+        "python multi_plot_avo.py \
+          --input_file ${input_file} \
+          --output_file ${output_file} \
+          --plot-dir ${plot_dir} \
+          --sample_name ${sample_name} \
+          --assay ${assay} \
+          --output_plot_file ${output_plot_file}"
+    done
+  done
+}
 
 #transform_to_wig
+test_multi_plot
+#test_plot
 #test
-run_avo
+#run_avo
